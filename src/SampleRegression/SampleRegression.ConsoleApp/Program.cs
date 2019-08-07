a156354799e9089fc14ac20d866c3a6969e56e08
wk-j <somnuk.wk@gmail.com>
2019-08-07 15:36:46 +0700

    Implement main function

--- src/SampleRegression/SampleRegression.ConsoleApp/Program.cs
+++ src/SampleRegression/SampleRegression.ConsoleApp/Program.cs
@@ -15,60 +15,31 @@ namespace SampleRegression.ConsoleApp
 {
     class Program
     {
-        //Machine Learning model to load and use for predictions
-        private const string MODEL_FILEPATH = @"MLModel.zip";
-
-        //Dataset to use for predictions 
-        private const string DATA_FILEPATH = @"/Users/wk/GitHub/MlTaxiFare/resource/taxi-fare-train.csv";
-
         static void Main(string[] args)
         {
-            MLContext mlContext = new MLContext();
+            var input = new ModelInput
+            {
+                Vendor_id = "CMT",
+                Rate_code = 1,
+                Passenger_count = 2,
+                Trip_time_in_secs = 800,
+                Trip_distance = 2,
+                Payment_type = "CRD"
+            };
 
-            // Training code used by ML.NET CLI and AutoML to generate the model
-            //ModelBuilder.CreateModel();
-
-            ITransformer mlModel = mlContext.Model.Load(GetAbsolutePath(MODEL_FILEPATH), out DataViewSchema inputSchema);
-            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
-
-            // Create sample data to do a single prediction with it 
-            ModelInput sampleData = CreateSingleDataSample(mlContext, DATA_FILEPATH);
-
-            // Try a single prediction
-            ModelOutput predictionResult = predEngine.Predict(sampleData);
-
-            Console.WriteLine($"Single Prediction --> Actual value: {sampleData.Fare_amount} | Predicted value: {predictionResult.Score}");
-
-            Console.WriteLine("=============== End of process, hit any key to finish ===============");
-            Console.ReadKey();
+            ConsumeModel(input);
         }
 
-        // Method to load single row of data to try a single prediction
-        // You can change this code and create your own sample data here (Hardcoded or from any source)
-        private static ModelInput CreateSingleDataSample(MLContext mlContext, string dataFilePath)
+        static void ConsumeModel(ModelInput input)
         {
-            // Read dataset to get a single row for trying a prediction          
-            IDataView dataView = mlContext.Data.LoadFromTextFile<ModelInput>(
-                                            path: dataFilePath,
-                                            hasHeader: true,
-                                            separatorChar: ',',
-                                            allowQuoting: true,
-                                            allowSparse: false);
+            var context = new MLContext();
+            var model = context.Model.Load($"./src/SampleRegression/SampleRegression.Model/MLModel.zip", out var schema);
+            var prediction = context.Model.CreatePredictionEngine<ModelInput, ModelOutput>(model);
 
-            // Here (ModelInput object) you could provide new test data, hardcoded or from the end-user application, instead of the row from the file.
-            ModelInput sampleForPrediction = mlContext.Data.CreateEnumerable<ModelInput>(dataView, false)
-                                                                        .First();
-            return sampleForPrediction;
-        }
+            var result = prediction.Predict(input);
 
-        public static string GetAbsolutePath(string relativePath)
-        {
-            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
-            string assemblyFolderPath = _dataRoot.Directory.FullName;
-
-            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
-
-            return fullPath;
+            var score = result.Score;
+            Console.WriteLine($"Text: {input.Vendor_id} | Predcition: {score}");
         }
     }
 }
